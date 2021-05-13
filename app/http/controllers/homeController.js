1ef847254ccb37e6f4170c92e6788f8161c8f2b8
shruti <shrutimahajan979@gmail.com>
2021-05-14 03:30:15 +0530

    initial

---
+++ app/http/controllers/homeController.js
@@ -0,0 +1,12 @@
+const Menu=require('../../models/menu')
+
+function homeController(){
+    return{
+        async index(req,res){
+            const burgers=await Menu.find()
+            
+            return res.render('home',{burgers:burgers})
+        }
+    }
+}
+module.exports=homeController
--- server.js
+++ server.js
@@ -1,29 +1,38 @@
+require('dotenv').config()
 const express=require('express')
 const app=express()
 const ejs=require('ejs')
 const path=require('path')
 const expressLayout=require('express-ejs-layouts')
 const PORT=process.env.PORT || 3000
+const mongoose=require('mongoose')
+const session=require('express-session')
+const flash=require('express-flash')
 
+const url='mongodb://localhost/burger';
+mongoose.connect(url,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true});
+const connection=mongoose.connection;
+connection.once('open',()=>{
+    console.log('Database Connected ..');
+}).catch(err=>{
+    console.log('Connection failed..')
+});
 
+app.use(session({
+    secret:process.env.COOKIES_SECRET,
+    resave:false,
+    saveUninitialized:false,
+store:mongoStore,
+cookie:{maxAge:1000*60*60*24}}))
+
+app.use(flash())
 app.use(express.static('public'))
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-    res.render('home')
-   })
-   app.get('/cart',(req,res)=>{
-       res.render('customers/cart')
-      })
-      app.get('/login',(req,res)=>{
-        res.render('auth/login')
-       })
-       app.get('/register',(req,res)=>{
-        res.render('auth/register')
-       })
-
+require('./routes/web')(app)
+   
 app.listen(PORT,()=>{
     console.log('hey')
 })
