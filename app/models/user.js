c36fdf1b046a6d6c47aaa11c2d7fb9bbe1d256f1
shruti <shrutimahajan979@gmail.com>
2021-05-15 15:43:50 +0530

    authentication

---
+++ app/models/user.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+
+const userSchema=new Schema({
+    name:{type:String ,require: true},
+     email:{type:String ,require: true ,unique:true},
+    password:{type:String ,require: true},
+    role:{type:String ,default:'buyer'},
+},{timestamps:true})
+
+const User=mongoose.model('User',userSchema)
+
+module.exports=User
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose=require('mongoose')
 const session=require('express-session')
 const flash=require('express-flash')
 const MongoDbStore=require('connect-mongo')
+const passport=require('passport')
 
 const url='mongodb://localhost/oxygen';
 mongoose.connect(url,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:true});
@@ -20,6 +21,8 @@ connection.once('open',()=>{
 });
 
 
+
+
 app.use(session({
     secret:process.env.COOKIES_SECRET,
     resave:false,
@@ -29,12 +32,19 @@ app.use(session({
     saveUninitialized:false,
 cookie:{maxAge:1000*60*60*24}}))
 
+const passportInit=require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(express.json())
 app.use((req,res,next)=>{
     res.locals.session=req.session
+    res.locals.user=req.user
     next()
  })
 
+ app.use(express.urlencoded({extended:false}))
 app.use(flash())
 app.use(express.static('public'))
 app.use(expressLayout)
